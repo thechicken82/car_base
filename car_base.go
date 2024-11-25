66cba6e0feffaeab742493a46be3bff82cd84817
thechicken82 <ethan.nadeau@outlook.com>
2024-11-24 22:01:47 -0600

    first commit

---
+++ car_base.go
@@ -0,0 +1,5 @@
+package car_base
+
+func Car_Base() string {
+	return "This is the car base, car calls this, and car_program calls car"
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thechicken82/car_base
+
+go 1.22.5
